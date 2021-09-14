33e7b9bb081ae96358328484da9551403e9085e9
AEUHHH <dr.codycash@gmail.com>
2021-09-13 21:49:11 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Cody Kalczuk
+ */
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is the password? ");
+
+        String password = input.nextLine();
+
+        if (password.equals("SuperSecret100"))
+        {
+            System.out.println("Welcome !");
+        } else {
+            System.out.println("I don't know you.");
+        }
+    }
+}
